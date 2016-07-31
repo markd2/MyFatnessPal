4dd9ca00ef2c26107b38c547130d63b9e6e76c2d
Mark Dalrymple <markd@bignerdranch.com>
2016-07-31 18:27:35 -0400

    Adds reading struct

--- MyFatnessPal/My Fatness Pal/AppDelegate.swift
+++ MyFatnessPal/My Fatness Pal/AppDelegate.swift
@@ -5,10 +5,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     
-    var weights = Weight.weightsFromArray([259.1, 260, 258.2, 259.7, 258.3])
+    var floatWeights = [259.1, 260, 258.2, 259.7, 258.3]
+    var dates = [(year: 2016, month: 7, day: 31),
+                 (year: 2016, month: 7, day: 30),
+                 (year: 2016, month: 7, day: 29),
+                 (year: 2016, month: 7, day: 28),
+                 (year: 2016, month: 7, day: 27)]
+    var readings = [Reading]()
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        print(weights)
+        let readings = Reading.readingsFrom(floatWeights: floatWeights, dateTuples: dates)
+        print("\(readings)")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
